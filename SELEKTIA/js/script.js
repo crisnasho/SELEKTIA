3728fcb84d3f04bebfc17801772978ff1732b504
crisnasho <169171326+crisnasho@users.noreply.github.com>
2025-09-12 22:24:20 -0300

    Delete SELEKTIA directory
    ya no lo necesito

--- SELEKTIA/js/script.js
+++
@@ -1,5 +0,0 @@
-// Captura el formulario y evita recargar la página
-document.getElementById("registroForm").addEventListener("submit", function(event) {
-  event.preventDefault(); 
-  alert("¡Registro enviado con éxito!");
-});
